3fb914319a359266c2509987b3e1f0cd8eaba02e
Sean Nealon <signalflowsean@gmail.com>
2018-10-22 15:18:23 -0400

    query that returns title and content in a pretty format

--- mongo-queries.js
+++ mongo-queries.js
@@ -1,3 +1,7 @@
+//mongo localhost:27017/noteful < mongo-queries.js
 'use strict';
 
-db.notes.find(); 
+//db.notes.find(); 
+
+db.notes.find().pretty(); 
+db.notes.find({}, {title: 1, content: 1}).pretty(); 
